b0284e7bbd2a71e84307374e5166a2bf85277e33
lauchlan <lauchlan.irwin@kelpieproject.com.au>
2018-05-23 02:24:13 +1000

    added exec.Command io capturing

--- main.go
+++ main.go
@@ -2,17 +2,74 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	stdio "io"
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
-
-	"github.com/lauchlan105/multiterm/multiterm"
 )
 
+type io struct {
+	in  pipe
+	out pipe
+}
+
+type pipe struct {
+	r *stdio.PipeReader
+	w *stdio.PipeWriter
+}
+
 func main() {
-	window := multiterm.Init()
-	defer window.Wait()
-	window.Start()
+
+	inR, inW := stdio.Pipe()
+	outR, outW := stdio.Pipe()
+
+	newPipe := io{
+		in: pipe{
+			inR,
+			inW,
+		},
+		out: pipe{
+			outR,
+			outW,
+		},
+	}
+
+	go runCommand([]string{"go", "run", "./maintwo/test.go"}, &newPipe)
+
+	//Print anything printed to the output pipe
+	go func() {
+		scanner := bufio.NewScanner(newPipe.out.r)
+		for scanner.Scan() {
+			fmt.Println("Output: " + scanner.Text())
+		}
+	}()
+
+	//Write any input to the input pipe
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			b, _ := reader.ReadBytes('\n')
+			newPipe.in.w.Write(b)
+		}
+	}()
+
+	/*
+		window := multiterm.Init()
+		defer window.Wait()
+		window.Start()
+	*/
+}
+
+func runCommand(s []string, io *io) {
+	cmd := exec.Command(s[0], s[1:]...)
+
+	cmd.Stdout = io.out.w
+	cmd.Stdin = io.in.r
+
+	cmd.Run()
 }
 
 func stayAlive() {
