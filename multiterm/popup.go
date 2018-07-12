93bb0f7755d7b96cad59dfad014397ba57887735
lauchlan <lauchlan.irwin@kelpieproject.com.au>
2018-07-12 12:58:41 +1000

    converted inline string to [][]rune slice

--- multiterm/popup.go
+++ multiterm/popup.go
@@ -4,7 +4,7 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
-//Position a
+//Position determines which corner the popups spawn
 type Position int
 
 const (
@@ -15,31 +15,41 @@ const (
 )
 
 //Print displays the content as a popup.
+//Background color defined by terminal.popupDefaultColor
 func (t *Terminal) Print(content interface{}) {
-
 	t.print(content.(string), t.popupDefaultColor)
-
 }
 
 //Error displays the content as a popup.
+//Background color defined by terminal.popupErrorColor
 func (t *Terminal) Error(content interface{}) {
-
 	t.print(content.(string), t.popupErrorColor)
-
 }
 
 func (t *Terminal) print(str string, bgColor termbox.Attribute) {
 
-	fmtStr := make([][]string, 0)
+	fmtStr := make([][]rune, 0)
 
 	//Create first line
-	fmtStr[0] = make([]string, 0)
+	fmtStr = append(fmtStr, make([]rune, 0))
+	longestRow := 0
 
 	for i, ch := range str {
 
 		if ch == '\n' {
 
+			//Re-check for longest row
+			currentRowLength := len(fmtStr[len(fmtStr)-1])
+			if currentRowLength > len(fmtStr[longestRow]) {
+				longestRow = i
+			}
+
+			fmtStr = append(fmtStr, make([]rune, 0))
+			continue
 		}
+
+		//Append current ch to end of the last row
+		fmtStr[len(fmtStr)-1] = append(fmtStr[len(fmtStr)-1], ch)
 
 	}
 
