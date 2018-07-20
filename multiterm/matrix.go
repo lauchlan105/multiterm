0230f06b9043c166463d6bdc7911d23aa505a69e
lauchlan <lauchlan.irwin@kelpieproject.com.au>
2018-07-20 22:31:52 +1000

    added error checking and block-fill prints

--- multiterm/matrix.go
+++ multiterm/matrix.go
@@ -1,6 +1,8 @@
 package multiterm
 
 import (
+	"strconv"
+
 	"github.com/nsf/termbox-go"
 )
 
@@ -24,27 +26,30 @@ func newMatrix() *Matrix {
 	}
 }
 
-func makeMatrix(str string, maxWidth int, maxHeight int) *Matrix {
+func makeMatrix(str string, maxWidth int, maxHeight int, bgColor termbox.Attribute, fillColor bool) (matrix *Matrix, err string) {
 
-	matrix := newMatrix()
+	matrix = newMatrix()
 	matrix.Content = append(matrix.Content, make([]termbox.Cell, 0))
+	longestRowIndex := -1
 
 	for _, ch := range str {
-
-		//if maxHeight -> break
-		if len(matrix.Content) == maxHeight {
-			break
-		}
 
 		//if last row doesn't exist -> make it
 		if len(matrix.Content) == 0 {
 			matrix.Content = append(matrix.Content, make([]termbox.Cell, 0))
 		}
 
+		//get length of latest row
+		currRowLength := len(matrix.Content[len(matrix.Content)-1])
+		if longestRowIndex == -1 {
+			longestRowIndex = len(matrix.Content) - 1
+		} else if currRowLength > len(matrix.Content[longestRowIndex]) {
+			longestRowIndex = len(matrix.Content) - 1
+		}
+
 		//if last row is full
 		//create a new row
 		if len(matrix.Content[len(matrix.Content)-1]) == maxWidth {
-
 			if ch == '\n' {
 				break
 			}
@@ -55,20 +60,52 @@ func makeMatrix(str string, maxWidth int, maxHeight int) *Matrix {
 		rowInd := len(matrix.Content) - 1
 
 		//Append char to current matrix row
-		matrix.Content[rowInd] = append(matrix.Content[rowInd], termbox.Cell{Ch: ch})
+		matrix.Content[rowInd] = append(matrix.Content[rowInd], termbox.Cell{Ch: ch, Bg: bgColor})
 
 		//If the current char is a newline,
 		//append whitespaces to the rest of the row
 		if ch == '\n' {
 			numberOfWhiteSpaces := maxWidth - len(matrix.Content[rowInd])
 			for numberOfWhiteSpaces > 0 {
-				matrix.Content[rowInd] = append(matrix.Content[rowInd], termbox.Cell{Ch: ' '})
+
+				if fillColor {
+					matrix.Content[rowInd] = append(matrix.Content[rowInd], termbox.Cell{Ch: ' ', Bg: bgColor})
+				} else {
+					matrix.Content[rowInd] = append(matrix.Content[rowInd], termbox.Cell{Ch: ' '})
+				}
+
 				numberOfWhiteSpaces--
 			}
 		}
 
 	}
 
-	return matrix
+	creationSuccess := true
+
+	//Set height and width
+	matrix.Height = len(matrix.Content)
+	if longestRowIndex >= 0 && longestRowIndex < len(matrix.Content) {
+		matrix.Width = len(matrix.Content[longestRowIndex])
+	} else {
+		creationSuccess = false
+		err += "longestRowIndex (" + strconv.Itoa(longestRowIndex) +
+			") out of bounds of matrix (" + strconv.Itoa(len(matrix.Content)) + ")\n"
+	}
+
+	if matrix.Height > maxHeight && maxHeight != -1 {
+		creationSuccess = false
+		err += "Matrix creation exceed height by " + strconv.Itoa(matrix.Height-maxHeight) + ".\n"
+	}
+
+	if matrix.Width > maxWidth && maxWidth != -1 {
+		creationSuccess = false
+		err += "Matrix creation exceed height by " + strconv.Itoa(matrix.Width-maxWidth) + ".\n"
+	}
+
+	if !creationSuccess {
+		err = "Failed to create matrix.\n" + err
+	}
+
+	return
 
 }
